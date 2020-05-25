7e94fe70501adf7fc829a071e274ca24235b9af0
dinrol <rakov0@mail.ru>
2020-05-25 14:49:58 +0300

    Заливка тест

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://listcarbrands.com/wp-content/uploads/2015/10/logo-Toyota.png" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://clck.ru/Ndbn3" alt=""/>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                My post
+                <div>
+                    New post
+                </div>
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
